81f3ad16481881e41bf8c65875482bec298502ac
Simon Uyttenhove <simon.uyttenhove@gmail.com>
2020-02-06 22:05:44 +0100

    debut usage de beautifulsoup

--- ImmoScanner/Travailleurs/Immoweb.py
+++ ImmoScanner/Travailleurs/Immoweb.py
@@ -4,51 +4,56 @@ from Moyens.ResultatsRechercheImmo import ResultatsRechercheImmo
 from Travailleurs.TravailleurImmo import TravailleurImmo
 from Moyens.RechercheImmo import RechercheImmo
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 
 
 class Immoweb(TravailleurImmo):
 
     def obtiens_resultats(self, recherche_immo: RechercheImmo):
+        resultats_recherche_immo = []
 
         url = self.create_url(recherche_immo)
         print(url)
 
         self.driver.get(url)
-        self.driver.implicitly_wait(5)
-
-        resultats_recherche_immo = []
-
-        resultats_page_web = self.driver.find_elements("main-content")
+        html = self.driver.page_source
 
         try:
-                resultats_recherche_immo.append(self.extraction_resultats(resultats_page_web))
+            resultats_recherche_immo.append(self.extraction_resultats(html))
 
         finally:
             self.driver.close()
             return resultats_recherche_immo
 
-    def extraction_resultats(self, resultats_page_web):
+    def extraction_resultats(self, html):
 
         resultat_recherche_immo = ResultatsRechercheImmo()
-        for result in resultats_page_web:
 
-            resultat_recherche_immo.id = result.get_attribute('id')
+        soup = BeautifulSoup(html, 'html.parser')
+        results_core = soup.find_all("div", {"card--result__body"})
+        results_media = soup.find_all("div", {"card--result__media"})
+
+        for result in results_core:
+
+            resultat_recherche_immo.id = soup.find("span", re.compile("card__information card--result__information card__information--property").text)
             print('id: ' + resultat_recherche_immo.id)
 
-            resultat_recherche_immo.description = result.find_element_by_tag_name('a').text
+            resultat_recherche_immo.description = result.get_attribute('card__description card--result__description')
             print('texte: ' + resultat_recherche_immo.description)
 
             resultat_recherche_immo.url = result.find_element_by_tag_name('a').get_attribute('href')
             print('lien:' + resultat_recherche_immo.url)
+
+            # spans = result.
             # print('prix:' + result.find_element_by_class_name('xl-price rangePrice')) il faut chercher plus loin (XPATH?) ou parser le texte descriptif
 
-            m = re.search(r'(\d+\.\d+)', resultat_recherche_immo.description)
-            if m is not None:
-                resultat_recherche_immo.prix = m.group(0)
-                print('prix:' + resultat_recherche_immo.prix)
+            #m = re.search(r'(\d+\.\d+)', resultat_recherche_immo.description)
+            #if m is not None:
+            #    resultat_recherche_immo.prix = m.group(0)
+            #    print('prix:' + resultat_recherche_immo.prix)
 
-            print('titre:' + result.find_element_by_tag_name('div').get_attribute('title'))
-
+           
         return resultat_recherche_immo
 
 
