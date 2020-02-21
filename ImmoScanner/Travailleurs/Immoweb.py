0b5c1dad8a8d57ab5ea8b13361325dc386e22e8b
Simon Uyttenhove <simon.uyttenhove@gmail.com>
2020-02-21 22:37:43 +0100

    ajout gestion nombre pages

--- ImmoScanner/Travailleurs/Immoweb.py
+++ ImmoScanner/Travailleurs/Immoweb.py
@@ -11,23 +11,29 @@ from price_parser import Price
 
 class Immoweb(TravailleurImmo):
 
+    def a_la_soupe(self, url):
+        self.driver.get(url)
+        html = self.driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+        return soup
+
     def obtiens_resultats(self, recherche_immo: RechercheImmo):
         resultats_recherche_immo = []
 
-        url = self.create_url(recherche_immo)
+        url = self.creation_url(recherche_immo)
         print(url)
-
-        self.driver.get(url)
-        html = self.driver.page_source
-
+        
         try:
+            soup = self.a_la_soupe(url)
+            nombre_pages = self.combien_pages(soup)
 
-            soup = BeautifulSoup(html, 'html.parser')
-            resultats_valeurs = soup.find_all("article", {"card card--result card--xl"} ) #results_core = soup.find_all("li", {"search-results__item"})
-            # results_media = soup.find_all("li", {"card--result__body"}) # soup.find_all("div", {"card--result__media"})
-
-            for resultat in resultats_valeurs:
-                resultats_recherche_immo.append(self.extraction_resultats(resultat))
+            for page in nombre_pages:
+                url = self.creation_url(recherche_immo, page)
+                soup = self.a_la_soupe(url)
+                resultats_valeurs = soup.find_all("article", {"card card--result card--xl"} )
+                       
+                for resultat in resultats_valeurs:
+                    resultats_recherche_immo.append(self.extraction_resultats(resultat))
 
         finally:
             self.driver.close()
@@ -45,10 +51,8 @@ class Immoweb(TravailleurImmo):
     def chopper_resultats_prix(self, resultat):
         prix = Price.fromstring(resultat.contents[0].contents[4].contents[0].contents[2].text.strip())
         return prix.amount, prix.currency
-
-
+    
     def extraction_resultats(self, resultat):
-
         resultat_recherche_immo = ResultatsRechercheImmo()
 
         resultat_recherche_immo.id = self.chopper_resultat_id(resultat)
@@ -65,34 +69,19 @@ class Immoweb(TravailleurImmo):
 
         return resultat_recherche_immo
 
-
-    def create_url(self, recherche_immo: RechercheImmo):
-
+    def creation_url(self, recherche_immo: RechercheImmo, page = 1):
         if recherche_immo.url is not "":
-            return recherche_immo.url
-        # https://www.immoweb.be/fr/recherche/maison/a-vendre/waterloo/1410
-        return f"https://www.immoweb.be/fr/recherche/{r_immo.type_bien}/{r_immo.louer_acheter}/{r_immo.ville}/{r_immo.code_postal}?countries=BE&page="
+            return recherche_immo.url # TODO gestion url et multipages
+        return f"https://www.immoweb.be/fr/recherche/{recherche_immo.type_bien}/{recherche_immo.louer_acheter}/{recherche_immo.ville}/{recherche_immo.code_postal}?countries=BE&page={page}"
 
-    def page_suivante(self, driver, url, nbre_pages):
+    def combien_pages(self, soup):
+        pagination = soup.find_all("a", {"pagination__link button button--text"}) 
+        if pagination.count != 0:
+            premiere_moitie = self.super_mouette_mouette(pagination)
+            return int(premiere_moitie[-1].text.split('Page', 1)[1].strip())
+        else:
+            return 1
 
-        page_suivante = False
-        url_test = url
-        url_test[:-1]
-        url_test += nbre_pages
-        try:
-            self.driver.get(url_test)
-            driver.find_element_by_class_name('navig-arrow-right')
-            page_suivante = True
-
-        except:
-            page_suivante = False
-
-        return page_suivante
-
-    def combien_pages(self, driver, url):
-
-        nbre_pages = 1
-        while self.page_suivante(driver, url, nbre_pages):
-            nbre_pages += 1
-
-        return nbre_pages
+    def super_mouette_mouette(self, la_liste):
+        moitie = len(la_liste) // 2
+        return la_liste[:moitie]
