73829be99214c033951e1c11f39b01ba168ed5f8
Simon Uyttenhove <simon.uyttenhove@gmail.com>
2020-08-19 20:39:10 +0200

    Added countries

---
+++ ImmoScanner/Countries/Country.py
@@ -0,0 +1,11 @@
+
+class Country():
+
+    def __init__():
+
+
+    def fetch_city_given_postal_code(self):
+    
+
+    def fetch_postal_code_given_city(self):
+
---
+++ ImmoScanner/Countries/Switzerland.py
@@ -0,0 +1,3 @@
+
+class Switzerland(Countrw):
+    
