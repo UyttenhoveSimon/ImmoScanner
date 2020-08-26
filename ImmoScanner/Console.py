fc730da3c1b747e739bf1d9bd5018fa1af8dd6df
Simon Uyttenhove <simon.uyttenhove@gmail.com>
2020-08-26 10:28:17 +0200

    started argparse, added immovlan no check

--- ImmoScanner/Console.py
+++ ImmoScanner/Console.py
@@ -1,47 +1,49 @@
 import sys
-from cmd import Cmd
-from argparse import ArgumentParser
-
+import argparse
+import logging
 from Means.Recherche import Recherche
 from Means.RealEstateResearch import RealEstateResearch
 from Workers.Immoweb import Immoweb
 from pprint import pprint
-import logging
 from Intellectuals.StatisticalInsights import StatisticalInsights
   
 
-class Console(Cmd):
+class Console():
 
-    parser = argparse.ArgumentParser(description='Welcome to ImmoScanner')
-
-    parser.add_argument(
-        'Country', metavar='str', type=str,
-        help='Country targeted by the research')
-
-    parser.add_argument(
-        'postal_code', metavar='str', type=str,
-        help='postal code targeted by the research')
-
-    parser.add_argument(
-        'city', metavar='str', type=str,
-        help='city targeted by the research')
-
-    parser.add_argument(
-        '--log', default=sys.stdout, type=argparse.FileType('w'),
-        help='the file where the sum should be written')
-    args = parser.parse_args()
-
-    intro = "Welcome to ImmoScanner"
-    logging.basicConfig(level=logging.DEBUG)
-
-    def do_research_immo(self, params):
+    def research_immo(self, args):
         scanner = ImmoScanner()
 
 
-    def do_research_immo_url(self, params):
-        
-
 if __name__ == '__main__':
-    console = Console()
-    console.prompt = '> '
-    Console().cmdloop()
+     parser = argparse.ArgumentParser(
+        description=
+        '''
+        Welcome to ImmoScanner, please enter:
+        -country (mandatory)
+        -postal_code (optional)
+        -city (optional)
+        -url (optional)
+        Add at least either the postal code or the city.
+        If url is provided, only the targeted website will be parsed, country must still be provided.
+        '''
+        )
+
+    parser.add_argument(
+        '--country', metavar='str', type=str,
+        help='Country targeted by the research')
+
+    parser.add_argument(
+        '--postal_code', metavar='str', type=str,
+        help='postal code targeted by the research')
+
+    parser.add_argument(
+        '--city', metavar='str', type=str,
+        help='city targeted by the research')
+
+    parser.add_argument(
+        '--url', metavar='str', type=str,
+        help='city targeted by the research')
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG)
