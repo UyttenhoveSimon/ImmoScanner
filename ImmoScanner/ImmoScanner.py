35a9d806335a75fc4d9cd47dfa88873c0e9e6d2d
Simon Uyttenhove <simon.uyttenhove@gmail.com>
2020-09-13 14:18:02 +0200

    started using geonqmes

--- ImmoScanner/ImmoScanner.py
+++ ImmoScanner/ImmoScanner.py
@@ -10,9 +10,7 @@ from Intellectuals.StatisticalInsights import StatisticalInsights
 from Countries.Country import Country
 
 
-class ImmoScanner():
-
-
+class ImmoScanner:
     def __init__(self):
         logging.basicConfig(level=logging.DEBUG)
 
@@ -27,37 +25,40 @@ class ImmoScanner():
         if not city:
             city = self.get_city(postal_code)
 
-
-        searches_immo_to_sell = RealEstateResearch(postal_code, city) # TODO: validate entries + research postal code or city name
+        searches_immo_to_sell = RealEstateResearch(
+            postal_code, city
+        )  # TODO: validate entries + research postal code or city name
         results_immoweb_to_sell = Immoweb().get_results(searches_immo_to_sell)
 
         good_stats_to_sell = StatisticalInsights(results_immoweb_to_sell)
         price_mean_to_sell = good_stats_to_sell.calculate_mean_price()
         price_median_to_sell = good_stats_to_sell.calculate_median_price()
 
-        searches_immo_to_rent = RealEstateResearch(postal_code, city) # TODO: validate entries + research postal code or city name
-        searches_immo_to_rent.rent_or_buy = "a-louer" # TODO : think how to orchestrate with another web site (deal with collisions) 
+        searches_immo_to_rent = RealEstateResearch(
+            postal_code, city
+        )  # TODO: validate entries + research postal code or city name
+        searches_immo_to_rent.rent_or_buy = "a-louer"  # TODO : think how to orchestrate with another web site (deal with collisions)
         results_immoweb_to_rent = Immoweb().get_results(searches_immo_to_rent)
 
         good_stats_to_rent = StatisticalInsights(results_immoweb_to_rent)
         price_mean_to_rent = good_stats_to_rent.calculate_mean_price()
         price_median_to_rent = good_stats_to_rent.calculate_median_price()
 
-        yield_rent_gross_median = ((price_median_to_rent * 12) / price_median_to_sell) * 100
-        logging.info(f'Rent yield gross median {yield_rent_gross_median}')
-                     
+        yield_rent_gross_median = (
+            (price_median_to_rent * 12) / price_median_to_sell
+        ) * 100
+        logging.info(f"Rent yield gross median {yield_rent_gross_median}")
+
     def research_real_estate_url(self, country, url):
         research = Research()
         research.url = url
         results_immoweb = Immoweb().get_results(research)
-    
 
     def get_country(self, country):
         return Country().generate_country_given_name(country)
-    
+
     def get_postal_code(self, city):
         return Country().fetch_postal_code_given_city(city)
 
     def get_city(self, postal_code):
         return Country().fetch_city_given_postal_code(postal_code)
-    
