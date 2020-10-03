fd66f5fe129c7a0c1fb187639e1edfebc6408861
Simon Uyttenhove <simon.uyttenhove@gmail.com>
2020-10-03 05:44:08 +0200

    changed a bit driver

---
+++ ImmoScanner/Countries/CountryFactory.py
@@ -0,0 +1,27 @@
+import pycountry
+from Countries.Belgium import Belgium
+from Countries.Switzerland import Switzerland
+from Countries.Country import Country
+
+
+class CountryFactory:
+    def __init__(self):
+        self.dicCountries = {"Belgium": Belgium(), "Switzerland": Switzerland()}
+
+    def generate_country_given_name(self, name):
+        if name in self.dicCountries.keys():
+            country = self.dicCountries[name]
+        else:
+            return None
+
+        iso_country = pycountry.countries.get(name=name)
+        country.alpha_2 = iso_country.alpha_2
+        country.alpha_3 = iso_country.alpha_3
+        country.numeric = iso_country.numeric
+        country.name = iso_country.name
+        country.official_name = iso_country.official_name
+        country.currency = pycountry.currencies.get(numeric=country.numeric)
+        country.languages = pycountry.languages.get(
+            alpha_2=f"{country.alpha_2.lower()}"
+        )
+        return country
