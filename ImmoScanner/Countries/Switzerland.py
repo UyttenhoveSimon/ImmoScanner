68951ab7a83c36fecae3ee275bf008e8a2f99449
UyttenhoveSimon <simon.uyttenhove@gmail.com>
2021-01-16 07:41:46 +0100

    started comparis

--- ImmoScanner/Countries/Switzerland.py
+++ ImmoScanner/Countries/Switzerland.py
@@ -1,10 +1,11 @@
 from Countries.Country import Country
 from Workers.Homegate import Homegate
+from Workers.Comparis import Comparis
 
 
 class Switzerland(Country):
     def __init__(self):
-        self.websites = [Homegate()]
+        self.websites = [Homegate(), Comparis()]
 
     def get_real_estate_websites(self):
         return self.websites
